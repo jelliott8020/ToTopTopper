c4ff8574cf0885940a044419bc9dc55e47f37388
Josh Elliott <jelliott8020@gmail.com>
2019-03-22 13:36:12 -0400

    Finished ToTopTopper

--- ToCodeLexer.java
+++ ToCodeLexer.java
@@ -15,40 +15,76 @@ public class ToCodeLexer extends StringLexer {
 		super(input);
 	}
 
+	public boolean isLower(char c) {
+		if (c <= 'z' && c >= 'a') {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isOther(char c) {
+		if ((c <= 'Z' && c >= 'A') || (c <= 9 && c >= 0) || c == '-' || c == '_') {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isBad(char c) {
+		if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A') || (c <= 9 && c >= 0) || c == '-' || c == '_') {
+			return false;
+		}
+		return true;
+	}
+
 	// required by Lexer interface
 	public Token nextToken() {
 		Token tok = Token.INIT_TOK;
 		if (!hasNextChar())
 			return Token.EOI_TOK;
 
-		int state = 1;
+		int state = 0;
 		while (hasNextChar()) {
 			char c = nextChar();
 
 			switch (state) {
-			case 1:
+			case 0:
 				if (c == 't')
+					state = 1;
+				else if (isLower(c))
+					state = 4;
+				else if (isOther(c) || isBad(c))
+					state = 5;
+				break;
+			case 1:
+				if (c == 'o')
 					state = 2;
-				else
+				else if (isLower(c) || isOther(c))
+					state = 4;
+				else if (isBad(c))
 					state = 5;
 				break;
 			case 2:
-				if (c == 'o')
+				if (c == 'p')
 					state = 3;
-				else
+				else if (isLower(c) || isOther(c))
+					state = 4;
+				else if (isBad(c)) {
 					state = 5;
+				}
 				break;
 			case 3:
-				if (c == 'p')
+				if (isLower(c) || isOther(c)) {
 					state = 4;
-				else
+				} else {
 					state = 5;
+				}
 				break;
 			case 4:
-				state = 5;
-				break;
-			case 5:
-				state = 5;
+				if (isLower(c) || isOther(c)) {
+					state = 4;
+				} else {
+					state = 5;
+				}
 				break;
 			default:
 				state = 5;
@@ -57,11 +93,14 @@ public class ToCodeLexer extends StringLexer {
 
 			// Deal with "final" states
 			switch (state) {
-			case 3:
+			case 2:
 				tok = Token.TO_TOK;
 				break;
-			case 4:
+			case 3:
 				tok = Token.TOP_TOK;
+				break;
+			case 4:
+				tok = Token.NAME_TOK;
 				break;
 			case 5:
 				return Token.ERR_TOK;
--- ToTableLexer.java
+++ ToTableLexer.java
@@ -11,25 +11,52 @@
 
 public class ToTableLexer extends StringLexer {
 
-	int[][] lexTable = { /* t o p ? */
-			/* state 1 */ { 2, 5, 5, 5 }, /* state 2 */ { 5, 3, 5, 5 }, /* state 3 */ { 5, 5, 4, 5 }, // Error "trap"
-																										// state
-			/* state 4 */ { 5, 5, 5, 5 }, /* state 5 */ { 5, 5, 5, 5 }, };
+	int[][] lexTable = { /* t o p a-z A-Z0-9_- err */
+			/* state 0 */ { 1, 4, 4, 4, 5, 5 }, /* state 1 */ { 4, 2, 4, 4, 4, 5 }, /* state 2 */ { 4, 4, 3, 4, 4, 5 },
+			/* state 3 */ { 4, 4, 4, 4, 4, 5 }, /* state 4 */ { 4, 4, 4, 4, 4, 5 },
+			/* state 5 */ { 5, 5, 5, 5, 5, 5 } };
 
 	public ToTableLexer(String input) {
 		super(input);
 	}
 
 	private int columnMap(char c) {
-		switch (c) {
-		case 't':
-			return 2;
-		case 'o':
-			return 3;
-		case 'p':
-			return 4;
+		// Prioritize to, top
+		// If find t, but space after, go to 5
+		int x = 0;
+
+		if (c == 't') {
+			x = 0;
+		} else if (c == 'o') {
+			x = 1;
+		} else if (c == 'p') {
+			x = 2;
+		} else if (c <= 'z' && c >= 'a') {
+			x = 3;
+		} else if ((c <= 'Z' && c >= 'A') || (c <= 9 && c >= 0) || c == '-' || c == '_') {
+			x = 4;
+		} else {
+			x = 5;
+		}
+
+		switch (x) {
+		// t
+		case 0:
+			return x;
+		// o
+		case 1:
+			return x;
+		// p
+		case 2:
+			return x;
+		// lower but not t o p
+		case 3:
+			return x;
+		// A-Z 0-9 _ -
+		case 4:
+			return x;
 		default:
-			return 5;
+			return x;
 		}
 	}
 
@@ -46,13 +73,17 @@ public class ToTableLexer extends StringLexer {
 
 			// Did we just enter a "final" state? Might need to do something
 			switch (state) {
-			case 3:
+			case 2:
 				tok = Token.TO_TOK;
 				break;
-			case 4:
+			case 3:
 				tok = Token.TOP_TOK;
+				break;
+			case 4:
+				tok = Token.NAME_TOK;
+				break;
 			case 5:
-				return Token.NAME_TOK;
+				return Token.ERR_TOK;
 			}
 		}
 		return tok;
